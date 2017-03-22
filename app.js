99df9153efc1a260622d55f378d148712bc54b2d
steven hunt <st3v3n.hunt@gmail.com>
2017-03-22 15:18:42 +0000

    :gem: Log all requests at debug

--- app.js
+++ app.js
@@ -4,12 +4,16 @@ const log = require('./lib/logger');
 const app = express();
 const port = process.env.PORT;
 
+app.use((req, res, next) => {
+  log.debug(req);
+  next();
+});
+
 app.get('/', (req, res) => {
   res.status(200).end();
 });
 
 app.get('/route-to-gp', (req, res) => {
-  log.debug(req);
   res.send(`Refering page: ${req.get('Referer')}`);
 });
 
