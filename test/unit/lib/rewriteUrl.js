da32e82cb3dfaf9a496c3faa9680ba1ffae79c2e
steven hunt <st3v3n.hunt@gmail.com>
2017-03-27 11:11:26 +0100

    :scissors: Trim additional query string params

--- test/unit/lib/rewriteUrl.js
+++ test/unit/lib/rewriteUrl.js
@@ -6,43 +6,56 @@ const expect = chai.expect;
 const choicesId = 40677;
 
 describe('rewriteUrl', () => {
-  it('should transform a GP url into the new GP profiles page', () => {
-    const gpReferer = `http://www.nhs.uk/Services/GP/Overview/DefaultView.aspx?id=${choicesId}`;
-    const expectedGpProfileUrl = `https://beta.nhs.uk/gp-surgeries/${choicesId}`;
+  describe('from GP pages', () => {
+    it('should transform a GP url into the new GP profiles page', () => {
+      const gpReferer = `http://www.nhs.uk/Services/GP/Overview/DefaultView.aspx?id=${choicesId}`;
+      const expectedGpProfileUrl = `https://beta.nhs.uk/gp-surgeries/${choicesId}`;
 
-    const rewrittenUrl = rewriteUrl(gpReferer);
+      const rewrittenUrl = rewriteUrl(gpReferer);
 
-    expect(rewrittenUrl).to.be.equal(expectedGpProfileUrl);
+      expect(rewrittenUrl).to.be.equal(expectedGpProfileUrl);
+    });
+
+    it('should transform a GP url with additional information into the new GP profiles page', () => {
+      const gpReferer = `http://www.nhs.uk/Services/GP/Overview/DefaultView.aspx?id=${choicesId}&additional=stuff`;
+      const expectedGpProfileUrl = `https://beta.nhs.uk/gp-surgeries/${choicesId}`;
+
+      const rewrittenUrl = rewriteUrl(gpReferer);
+
+      expect(rewrittenUrl).to.be.equal(expectedGpProfileUrl);
+    });
   });
 
-  it('should return undefined when the url is not a GP page from choices', () => {
-    const nonGpReferer = 'http://www.nhs.uk/Services/hospitals/Overview/DefaultView.aspx?id=2205';
+  describe('from non GP pages', () => {
+    it('should return undefined when the url is not a GP page from choices', () => {
+      const nonGpReferer = 'http://www.nhs.uk/Services/hospitals/Overview/DefaultView.aspx?id=2205';
 
-    const result = rewriteUrl(nonGpReferer);
+      const result = rewriteUrl(nonGpReferer);
 
-    // eslint-disable-next-line no-unused-expressions
-    expect(result).to.be.undefined;
-  });
+      // eslint-disable-next-line no-unused-expressions
+      expect(result).to.be.undefined;
+    });
 
-  it('should return undefined when the url does not contain an id', () => {
-    const noIdGpReferer = 'http://www.nhs.uk/Services/gp/Overview/DefaultView.aspx?';
-    const result = rewriteUrl(noIdGpReferer);
+    it('should return undefined when the url does not contain an id', () => {
+      const noIdGpReferer = 'http://www.nhs.uk/Services/gp/Overview/DefaultView.aspx?';
+      const result = rewriteUrl(noIdGpReferer);
 
-    // eslint-disable-next-line no-unused-expressions
-    expect(result).to.be.undefined;
-  });
+      // eslint-disable-next-line no-unused-expressions
+      expect(result).to.be.undefined;
+    });
 
-  it('should return undefined when the url is null', () => {
-    const result = rewriteUrl(null);
+    it('should return undefined when the url is null', () => {
+      const result = rewriteUrl(null);
 
-    // eslint-disable-next-line no-unused-expressions
-    expect(result).to.be.undefined;
-  });
+      // eslint-disable-next-line no-unused-expressions
+      expect(result).to.be.undefined;
+    });
 
-  it('should return undefined when the url is blank', () => {
-    const result = rewriteUrl('');
+    it('should return undefined when the url is blank', () => {
+      const result = rewriteUrl('');
 
-    // eslint-disable-next-line no-unused-expressions
-    expect(result).to.be.undefined;
+      // eslint-disable-next-line no-unused-expressions
+      expect(result).to.be.undefined;
+    });
   });
 });
