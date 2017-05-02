10b15e35d28075e6f29262fed0c85fbdedd3a680
steven hunt <st3v3n.hunt@gmail.com>
2017-05-02 12:40:42 +0100

    :ship: Add default port in app now not set in Dockerfile

--- app-start.js
+++ app-start.js
@@ -1,7 +1,7 @@
 const log = require('./lib/logger');
 const app = require('./app');
 
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   log.info(`App listening on port ${port}`);
