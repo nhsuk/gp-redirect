7578281a49ca1649aadf72c15a083d9e12482144
steven hunt <st3v3n.hunt@gmail.com>
2017-07-05 17:12:35 +0100

    :lock: Update yarn.lock on Greenkeeper PRs

--- test/integration/app.js
+++ test/integration/app.js
@@ -15,52 +15,52 @@ describe('app', () => {
 
   it('should redirect root requests to the SITE_ROOT', (done) => {
     chai.request(app)
-    .get('/')
-    .end((err, res) => {
+      .get('/')
+      .end((err, res) => {
       // eslint-disable-next-line no-unused-expressions
-      expect(res).to.redirect;
-      expect(res.redirects[0]).to.have.string(SITE_ROOT);
-      expect(res).to.have.status(200);
-      done();
-    });
+        expect(res).to.redirect;
+        expect(res.redirects[0]).to.have.string(SITE_ROOT);
+        expect(res).to.have.status(200);
+        done();
+      });
   });
 
   it('should redirect to the new gp profile page when referer comes from gp on choices', (done) => {
     chai.request(app)
-    .get(SITE_ROOT)
-    .set('referer', choicesGpPage)
-    .end((err, res) => {
+      .get(SITE_ROOT)
+      .set('referer', choicesGpPage)
+      .end((err, res) => {
       // eslint-disable-next-line no-unused-expressions
-      expect(res).to.redirect;
-      expect(res).to.redirectTo(redirectedToUrl);
-      done();
-    });
+        expect(res).to.redirect;
+        expect(res).to.redirectTo(redirectedToUrl);
+        done();
+      });
   });
 
   it('should display the options page when no referer is available', (done) => {
     chai.request(app)
-    .get(SITE_ROOT)
-    .end((err, res) => {
+      .get(SITE_ROOT)
+      .end((err, res) => {
       // eslint-disable-next-line no-unused-expressions
-      expect(res).to.be.html;
-      expect(res).to.have.status(200);
-      done();
-    });
+        expect(res).to.be.html;
+        expect(res).to.have.status(200);
+        done();
+      });
   });
 
   it('should display debug json when debug param supplied', (done) => {
     chai.request(app)
-    .get(SITE_ROOT)
-    .set('referer', choicesGpPage)
-    .query({ debug: '' })
-    .end((err, res) => {
-      expect(res).to.have.status(200);
-      // eslint-disable-next-line no-unused-expressions
-      expect(res).to.be.json;
-      // eslint-disable-next-line no-unused-expressions
-      expect(res.body.headers).to.not.be.undefined;
-      expect(res.body.redirectTo).to.be.equal(redirectedToUrl);
-      done();
-    });
+      .get(SITE_ROOT)
+      .set('referer', choicesGpPage)
+      .query({ debug: '' })
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        // eslint-disable-next-line no-unused-expressions
+        expect(res).to.be.json;
+        // eslint-disable-next-line no-unused-expressions
+        expect(res.body.headers).to.not.be.undefined;
+        expect(res.body.redirectTo).to.be.equal(redirectedToUrl);
+        done();
+      });
   });
 });
